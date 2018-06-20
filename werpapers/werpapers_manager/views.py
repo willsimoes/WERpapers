bc53e598184e02cbf1af1c6e07f44b715beb1601
Willian Simões Pinto <willian.smos@gmail.com>
2018-06-19 23:25:47 -0300

    adicionando pagina de papers por conferencia

--- werpapers/werpapers_manager/views.py
+++ werpapers/werpapers_manager/views.py
@@ -5,5 +5,6 @@ context = {}
 
 def index(request):
     return render(request, 'werpapers_manager/index.html', context)
-def detail(request, question_id):
-    return HttpResponse("You're looking at question %s." % question_id)
+def papers_by_conference(request, conference):
+    context["conference"] = conference
+    return render(request, 'werpapers_manager/papers_by_conference.html', context)
