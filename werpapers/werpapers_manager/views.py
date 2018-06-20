37dbd3c89cd161903498ca592b6663636c1a01b0
Willian Simões Pinto <willian.smos@gmail.com>
2018-06-19 21:58:17 -0300

    fazendo view de hello world

--- werpapers/werpapers_manager/views.py
+++ werpapers/werpapers_manager/views.py
@@ -1,3 +1,9 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
-# Create your views here.
+context = {}
+
+def index(request):
+    return render(request, 'werpapers_manager/index.html', context)
+def detail(request, question_id):
+    return HttpResponse("You're looking at question %s." % question_id)
